8ceda32adab0d741bfb63b543746e90e54384e4d
xiongbo <xiongbo027@gmail.com>
2012-11-02 12:33:53 +0800

    add memberdetails with corp and agency
    在会员的详细信息中增加了企业和机构的信息维护功能

---
+++ GGZBTQPT_PRO/Areas/Member/Controllers/AgencyController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GGZBTQPT_PRO.Models;
+
+namespace GGZBTQPT_PRO.Areas.Member.Controllers
+{ 
+    public class AgencyController : Controller
+    {
+        private GGZBTQPTDBContext db = new GGZBTQPTDBContext();
+ 
+ 
+        public void BindAgencyType(object select = null)
+        {
+            List<T_PTF_DicDetail> AgencyType = db.T_PTF_DicDetail.Where(p => (p.DicType == "JG01" && p.IsValid == "1")).ToList();
+
+            ViewData["AgencyType"] = new SelectList(AgencyType, "ID", "Name", select);
+        }
+        public void BindArea(object select = null)
+        {
+            List<T_PTF_DicTreeDetail> Area = db.T_PTF_DicTreeDetail.Where(p => (p.DicType == "34" && p.Depth == 1)).ToList();
+
+            ViewData["Province"] = new SelectList(Area, "ID", "Name", select);
+        }
+
+        
+        //
+        // GET: /JG_Agency/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            T_JG_Agency t_jg_agency = db.T_JG_Agency.Find(id);
+            BindArea(t_jg_agency.Province);
+            BindAgencyType(t_jg_agency.AgencyType);
+
+            return PartialView(t_jg_agency);
+        }
+
+        //
+        // POST: /JG_Agency/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(T_JG_Agency t_jg_agency)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(t_jg_agency).State = EntityState.Modified;
+                db.SaveChanges();
+                return Json(new { statusCode = "200", message = "信息保存成功！", type = "success" });
+            }
+            return Json(new { statusCode = "200", message = "信息保存失败！", type = "error" }); 
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ GGZBTQPT_PRO/Areas/Member/Controllers/CorpController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GGZBTQPT_PRO.Models;
+using System.IO;
+
+namespace GGZBTQPT_PRO.Areas.Member.Controllers
+{ 
+    public class CorpController : Controller
+    {
+        private GGZBTQPTDBContext db = new GGZBTQPTDBContext();
+
+
+
+        public void BindProperty(object select = null)
+        {
+            List<T_PTF_DicDetail> Property = db.T_PTF_DicDetail.Where(p => (p.DicType == "5")).ToList();
+
+            ViewData["Property"] = new SelectList(Property, "ID", "Name", select);
+        }
+        public void BindIndustry(object select = null)
+        {
+            List<T_PTF_DicDetail> Industry = db.T_PTF_DicDetail.Where(p => (p.DicType == "XM01")).ToList();
+
+            ViewData["Industry"] = new SelectList(Industry, "ID", "Name", select);
+        }
+
+        public void BindArea(object select = null)
+        {
+            List<T_PTF_DicTreeDetail> Area = db.T_PTF_DicTreeDetail.Where(p => (p.DicType == "34" && p.Depth == 1)).ToList();
+
+            ViewData["Province"] = new SelectList(Area, "ID", "Name", select);
+        }
+        public void BindStage(object select = null)
+        {
+            List<T_PTF_DicDetail> Stage = db.T_PTF_DicDetail.Where(p => (p.DicType == "QY01")).ToList();
+
+            ViewData["Stage"] = new SelectList(Stage, "ID", "Name", select);
+        }
+
+        
+        //
+        // GET: /QY_Corp/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            T_QY_Corp t_qy_corp = db.T_QY_Corp.Find(id);
+
+            BindStage(t_qy_corp.Stage);
+            BindArea(t_qy_corp.Province);
+            BindIndustry(t_qy_corp.Industry);
+            BindProperty(t_qy_corp.Property);
+
+            return PartialView(t_qy_corp);
+        }
+
+        //
+        // POST: /QY_Corp/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(T_QY_Corp t_qy_corp, FormCollection collection)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(t_qy_corp).State = EntityState.Modified;
+                t_qy_corp.UpdateTime = DateTime.Now;
+                Stream stream = Request.Files.Count > 0
+                                        ? Request.Files[0].InputStream
+                                        : Request.InputStream;
+                //存入文件
+                if (stream.Length > 0)
+                {
+                    t_qy_corp.Logo = new byte[stream.Length];
+                    stream.Read(t_qy_corp.Logo, 0, t_qy_corp.Logo.Length);
+                }
+                string cyear = collection["FYear"].ToString();
+                if (db.T_QY_Financial.Where(f => (f.CorpID == t_qy_corp.ID && f.CurYear == cyear)).Count() > 0)
+                {
+                    db.T_QY_Financial.Where(f => f.CurYear == cyear).FirstOrDefault().TotalAssets = Convert.ToDecimal(collection["TotalAssets"]);
+                    db.T_QY_Financial.Where(f => f.CurYear == cyear).FirstOrDefault().Revenue = Convert.ToDecimal(collection["Revenue"]);
+                }
+                else
+                {
+                    T_QY_Financial financial = new T_QY_Financial();
+                    financial.CorpID = t_qy_corp.ID;
+                    financial.CurYear = cyear;
+                    financial.TotalAssets = Convert.ToDecimal(collection["TotalAssets"]);
+                    financial.Revenue = Convert.ToDecimal(collection["Revenue"]); ;
+                    db.T_QY_Financial.Add(financial);
+                }
+                if (db.T_QY_Product.Where(f => f.CorpID == t_qy_corp.ID).Count() > 0)
+                {
+                    db.T_QY_Product.Where(f => f.CorpID == t_qy_corp.ID).FirstOrDefault().ProductName = collection["ProductName"];
+                    db.T_QY_Product.Where(f => f.CorpID == t_qy_corp.ID).FirstOrDefault().Content = collection["Content"];
+                }
+                else
+                {
+                    T_QY_Product Product = new T_QY_Product();
+                    Product.CorpID = t_qy_corp.ID;
+                    Product.ProductName = collection["ProductName"];
+                    Product.Content = collection["Content"];
+                    db.T_QY_Product.Add(Product);
+                }
+                db.SaveChanges();
+                return Json(new { statusCode = "200", message = "信息保存成功！", type = "success" });
+            }
+            return Json(new { statusCode = "200", message = "信息保存失败！", type = "error" });
+        }
+
+        public ActionResult UploadLogo(string qqfile)
+        {
+            return Json(new { statusCode = "200", message = "信息保存成功！", type = "success" });
+        }
+   
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
