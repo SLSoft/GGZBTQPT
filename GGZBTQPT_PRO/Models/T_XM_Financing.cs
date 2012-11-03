ba790b6a9d6261b74adc3b9548374133b9284f7b
xiongbo <xiongbo027@gmail.com>
2012-11-03 19:51:32 +0800

    update memberbrief with dynamic_info

--- GGZBTQPT_PRO/Models/T_XM_Financing.cs
+++ GGZBTQPT_PRO/Models/T_XM_Financing.cs
@@ -60,10 +60,7 @@ namespace GGZBTQPT_PRO.Models
         public Nullable<System.DateTime> UpdateTime { get; set; }
 
 
-        public int MemberID { get; set; }
-        //-------TO-DO------------
-        //这里的项目应该和会员是一对多的关系，每个会员都能发布多个项目，需要增加对应的映射关系
-
+        public int MemberID { get; set; } 
         public virtual T_HY_Member Member { get; set; }
 
         public virtual ICollection<T_HY_Favorite> Favoites { get; set; }//每个项目都对应多个收藏
