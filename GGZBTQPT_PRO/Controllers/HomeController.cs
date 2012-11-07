f89d9b8f495d2c117496cd9d4855f78fc63710e3
xiongbo <xiongbo027@gmail.com>
2012-11-07 16:33:32 +0800

    add dynamic systemlinks

--- GGZBTQPT_PRO/Controllers/HomeController.cs
+++ GGZBTQPT_PRO/Controllers/HomeController.cs
@@ -7,13 +7,35 @@ using GGZBTQPT_PRO.Models;
 
 namespace GGZBTQPT_PRO.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
-        private GGZBTQPTDBContext db = new GGZBTQPTDBContext();
+
         public ActionResult Index()
         {
             var menus = db.T_ZC_Menu.Where(p => p.IsValid == true).ToList();
             return View(menus);
         }
+
+        public ActionResult SystemLink()
+        {
+            var system_links = db.T_ZC_System.OrderBy(s => s.ID).ToList();
+            return PartialView(system_links);
+        }
+
+        public ActionResult SystemMenus(int id = 1)
+        {
+            var system = FindSystemByID(id);
+            var menu_links = system.Menus.OrderBy(m => m.ID).ToList();
+            return PartialView(menu_links);
+        }
+
+        //
+        //Helper
+        public T_ZC_System FindSystemByID(int id)
+        {
+            var system = db.T_ZC_System.Find(id);
+            return system;
+        }
+
     }
 }
